846221e17800c4826aa86fdfce277e0e0248ec41
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-15 16:55:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function nestedTarget() {
  function increaseRankBy(n) {
    let rankedList = document.querySelectorAll(`.ranked-list`)
    
-   for (i=0, l=rankedList.length; i<l; i++) {
+   for (let i=0, l=rankedList.length; i<l; i++) {
      let children = rankedList[i].children
-     for (j=0, c = children.length; j<c; j++) {
+     for (let j=0, c = children.length; j<c; j++) {
        children[j].innerHTML = parseInt(children[j].innerHTML) + n }
    }
  }
