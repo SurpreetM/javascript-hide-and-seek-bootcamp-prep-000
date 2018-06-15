31053e0092f6e30c6d0ab23970f253b3a377dc6d
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-15 16:38:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function nestedTarget() {
    
    for (i=0, l=rankedList.length; i<l; i++) {
      let children = rankedList[i].children
-     for (i=0, l=)
+     for (i=0, c = children.length; i<c; i++) {
+       
+     }
    
  }
